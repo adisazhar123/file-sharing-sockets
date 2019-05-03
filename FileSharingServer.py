5d4f0c53c1c8912143d9ebafe60770716e1ca60b
adis azhar <adisazhar123@gmail.com>
2019-05-04 01:09:01 +0700

    Init file sharing server

---
+++ FileSharingServer.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+import os
+
+
+class FileSharingServer:
+    # Constructor receives host & port as tuple,
+    # data port for data connection
+    def __init__(self, (host, port), data_port):
+        self.host = host
+        self.port = port
+        self.data_port = data_port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def start_socket(self):
+        server_address = (self.host, self.port)
+
+        try:
+            print 'Creating server socket ', server_address
+            self.socket.bind(server_address)
+            self.socket.listen(10)
+            print 'Server is listening on ', server_address
+        except Exception, e:
+            print 'Failed to create server on ', server_address, ' because ', str(e.message)
+            quit()
+
+    def start(self):
+        self.start_socket()
+
+        try:
+            while True:
+                print 'Waiting for connection'
+                # TODO: Start thread class FileSharingThread
+                # pass in current accepted socket
+                thread = FileSharingThread(self.socket.accept(), self.data_port)
+                thread.daemon = True
+                thread.start()
+        except KeyboardInterrupt:
+            print 'Closing socket connection'
+            self.socket.close()
+            quit()
