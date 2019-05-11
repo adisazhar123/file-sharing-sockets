1453646bf71a639d2a18b50e1016c7b6f065a051
adis azhar <adisazhar123@gmail.com>
2019-05-11 23:24:06 +0700

    authenticate[done], download file [done], download directory[wip]

--- Gui.py
+++ Gui.py
@@ -2,13 +2,10 @@ import Tkinter as tk
 import ttk
 from threading import *
 from client.FileSharingClient import Client
-import os
 from Tkinter import *
 import tkSimpleDialog
+import tkFileDialog
 
-# tree.insert(folder1, "end", text="photo1.png", values=("23-Jun-17 11:28","PNG file","2.6 KB"))
-# tree.insert(folder1, "end", text="photo2.png", values=("23-Jun-17 11:29","PNG file","3.2 KB"))
-# tree.insert(folder1, "end", text="photo3.png", values=("23-Jun-17 11:30","PNG file","3.1 KB"))
 HOST = '127.0.0.1'
 PORT = 1337
 DATA_PORT = 1338
@@ -20,38 +17,85 @@ class App(Thread):
         self.fsc = Client((HOST, PORT), DATA_PORT, self)
 
         self.master = master
-        self.menubar = Menu(master)
+        self.master.title('File Sharing Sockets')
+        self.frame = Frame(master)
+        self.frame.pack()
+
+        self.construct_top_menu()
+        self.construct_tree(self.master)
+
+        self.menubar.entryconfig("Actions", state="disabled")
+
+        self.popup = Menu(self.master, tearoff=0)
+        self.popup.add_command(label="Download", command=self.browse_directory)
+        self.popup.add_command(label="Share")
+        self.popup.add_separator()
+
+        # variable to hold download directory location
+        self.download_location = ''
+        # variable to hold the file/ dir to be downloaded
+        self.to_download = ''
+
+    def construct_top_menu(self):
+        self.menubar = Menu(self.frame)
+
         self.filemenu = Menu(self.menubar, tearoff=0)
         self.filemenu.add_command(label="Create Directory", command=self.MKDIR)
         self.filemenu.add_command(label="Refresh", command=self.LIST)
         self.filemenu.add_command(label="Save")
         self.filemenu.add_separator()
         self.filemenu.add_command(label="Exit")
+
+        self.filemenu2 = Menu(self.menubar, tearoff=0)
+        self.filemenu2.add_command(label="Login", command=self.AUTHENTICATE)
+
         self.menubar.add_cascade(label="Actions", menu=self.filemenu)
+        self.menubar.add_cascade(label="Account", menu=self.filemenu2)
         self.master.config(menu=self.menubar)
 
+    def construct_tree(self, master):
         self.tree = ttk.Treeview(master)
-
-        self.text = 'initial text'
-
         self.tree["columns"] = ("one", "two", "three")
-        self.tree.column("#0", width=270, minwidth=270, stretch=tk.NO)
-        self.tree.column("one", width=150, minwidth=150, stretch=tk.NO)
-        self.tree.column("two", width=400, minwidth=200)
-        self.tree.column("three", width=80, minwidth=50, stretch=tk.NO)
+        self.tree.column("#0", width=270, minwidth=270, stretch=tk.YES)
+        self.tree.column("one", width=150, minwidth=150, stretch=tk.YES)
+        self.tree.column("two", width=150, minwidth=150)
+        self.tree.column("three", width=80, minwidth=50, stretch=tk.YES)
 
-        self.tree.heading("#0",text="Name",anchor=tk.W)
-        self.tree.heading("one", text="Date modified",anchor=tk.W)
-        self.tree.heading("two", text="Type",anchor=tk.W)
-        self.tree.heading("three", text="Size",anchor=tk.W)
+        self.tree.heading("#0", text="Name", anchor=tk.W)
+        self.tree.heading("one", text="Date modified", anchor=tk.W)
+        self.tree.heading("two", text="Type", anchor=tk.W)
+        self.tree.heading("three", text="Size", anchor=tk.W)
+        self.tree.pack(side=tk.TOP, fill=tk.X)
 
-        self.tree.pack(side=tk.TOP,fill=tk.X)
+        self.tree.bind("<Button-3>", self.do_popup)
 
-    def hahaha(self, e):
-        curItem = self.tree.focus()
-        print self.tree.item(curItem)
-        print 'after'
-        print self.text
+    def do_popup(self, event):
+        iid = self.tree.identify_row(event.y)
+        try:
+            if iid:
+                self.tree.selection_set(iid)
+                self.popup.tk_popup(event.x_root + 50, event.y_root, 0)
+                selected_item = self.tree.item(iid)
+                self.to_download = selected_item['text']
+                print 'clicked on', self.tree.item(iid)
+        finally:
+            self.popup.grab_release()
+
+    # opens a browser directory to save download folder/file
+    def browse_directory(self):
+        dir_name = tkFileDialog.askdirectory()
+        if dir_name:
+            self.download_location = dir_name
+            self.DOWNLOAD(self.to_download)
+            print dir_name
+
+    def AUTHENTICATE(self):
+        username = tkSimpleDialog.askstring("Username", "Enter username")
+        password = tkSimpleDialog.askstring("Password", "Enter password")
+        self.fsc.AUTHENTICATE(username, password)
+
+    def authenticated(self):
+        self.menubar.entryconfig("Actions", state="normal")
 
     def LIST(self):
         print 'in LIST CLICK'
@@ -60,12 +104,14 @@ class App(Thread):
     def MKDIR(self):
         print 'in MKDIR Gui'
         dir_name = tkSimpleDialog.askstring("Name of directory", "Enter directory name")
-        self.fsc.MKDIR(dir_name)
+        if dir_name:
+            self.fsc.MKDIR(dir_name)
+
+    def DOWNLOAD(self, file_dir_name):
+        print file_dir_name
+        self.fsc.DOWNLOAD(file_dir_name)
 
     def run(self):
-        print 'in thread'
-        print 'before'
-        print self.text
         self.fsc.start()
 
     def reconstruct_tree(self, files_dirs):
--- database/seeder.py
+++ database/seeder.py
@@ -1,14 +1,15 @@
 import sqlite3
+import os
 conn = sqlite3.connect('file_sharing_sockets.db')
 
 c = conn.cursor()
 print 'seeding users'
 
-users = [(1, 'adis', 'password123', 'adis/'),
-        (2, 'bani', 'password123', 'bani/'),
-        (3, 'rifqi', 'password123', 'rifqi/'),
-        (4, 'bayu', 'password123', 'bayu/'),
-        (5, 'fahrizal', 'password123', 'fahrizal/'),
+users = [(1, 'adis', 'password123', 'adis'),
+        (2, 'bani', 'password123', 'bani'),
+        (3, 'rifqi', 'password123', 'rifqi'),
+        (4, 'bayu', 'password123', 'bayu'),
+        (5, 'fahrizal', 'password123', 'fahrizal'),
         ]
 
 c.executemany('INSERT INTO user VALUES (?,?,?,?)', users)
@@ -17,3 +18,10 @@ print 'seeded users'
 conn.commit()
 
 conn.close()
+
+core_dir = os.getcwd() + '/../core'
+# init user dirs
+for user in users:
+        if not os.path.exists(core_dir + '/' + user[3]):
+                print 'making directory ' +user[3]
+                os.makedirs(core_dir + '/' + user[3])
